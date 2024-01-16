352eb56d206ee23ced33f5d5b796d57ed9b3c8fc
= <=>
2024-01-16 13:54:58 +0530

    added app functionality

--- app.py
+++ app.py
@@ -1,5 +1,21 @@
-# db_initializer.py
-from db_operations.db_initializer import create_table, insert_records
+from src.gemini_wrapper import get_response_as_sql_query, get_query_response_as_text, configure_gemini
+from utils.prompts import system_prompt
+import streamlit as st
 
-create_table()
-insert_records()
+configure_gemini()
+# a = get_response_as_sql_query(user_prompt="how many total student are there",system_prompt=system_prompt)
+# print(a)
+# print(get_query_response_as_text(a))
+
+st.set_page_config(page_title="Text to SQL query Generator")
+st.header("Database Retriever")
+question = st.text_input("Input: ", key="input")
+submit = st.button("Ask the question")
+
+if submit:
+    response_sql = get_response_as_sql_query(question,system_prompt)
+    data = get_query_response_as_text(response_sql)
+    st.subheader("The response is: ")
+    for row in data:
+        print(row)
+        st.write(row)
